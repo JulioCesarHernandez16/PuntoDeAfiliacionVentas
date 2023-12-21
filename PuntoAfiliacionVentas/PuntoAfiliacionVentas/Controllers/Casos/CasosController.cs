565401478ae3a3b62b229283258f812c180582b7
Julio César Hernández <julio.hernandez@telecablecr.com>
2023-12-21 14:56:42 -0600

    Segundo commit, diseño finalizado

---
+++ PuntoAfiliacionVentas/PuntoAfiliacionVentas/Controllers/Casos/CasosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PuntoAfiliacionVentas.Controllers.Casos
+{
+    public class CasosController : Controller
+    {
+        // GET: Casos
+        public ActionResult Casos()
+        {
+            return View();
+        }
+    }
+}
